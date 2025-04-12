c3cfa6d09b00c5d92aed1c29744508b03799f089
seria <seria.ati@gmail.com>
2025-04-13 08:05:19 +0900

    feat: Log Discord webhook responses

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
+from loguru import logger
 import requests
 
 
 def discord_webhook(content: str, *, url: str) -> None:
-    requests.post(url, json={"content": content})
+    resp = requests.post(url, json={"content": content})
+    logger.info(f"Discord webhook response: {resp.status_code} {resp.text}")
